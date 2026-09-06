9673bbb94426981ec2333e07208c54a8e2309039
Marwan Saif <merosaif837@gmail.com>
2026-09-06 21:16:02 +0300

    first commit

---
+++ imdbfake_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from imdbfake_app.models import Movie
+
+# Create your views here.
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    data = {'movies': list(movies.values())}
+    return JsonResponse(data)
