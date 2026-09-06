0160504b84215c6bffaea8dfdb011f11150d2ddc
Marwan Saif <merosaif837@gmail.com>
2026-09-07 01:20:54 +0300

    installing and preparing DRF

--- imdbfake/urls.py
+++ imdbfake/urls.py
@@ -19,5 +19,5 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('imdbfake_app.urls')),
+    path('', include('imdbfake_app.api.urls')),
 ]
--- imdbfake_app/views.py
+++ imdbfake_app/views.py
@@ -1,21 +1,21 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from imdbfake_app.models import Movie
+# from django.shortcuts import render
+# from django.http import JsonResponse
+# from imdbfake_app.models import Movie
 
-# Create your views here.
+# # Create your views here.
 
-def movie_list(request):
-    movies = Movie.objects.all()
-    data = {'movies': list(movies.values())}
-    return JsonResponse(data)
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     data = {'movies': list(movies.values())}
+#     return JsonResponse(data)
 
-def movie_detail(request, pk):
-    movie = Movie.objects.get(pk=pk)
-    data = {'movie': {
-                'id': movie.id,
-                'title': movie.name,
-                'description': movie.description,
-                # 'release_date': movie.release_date,
-            }}
-    return JsonResponse(data)
+# def movie_detail(request, pk):
+#     movie = Movie.objects.get(pk=pk)
+#     data = {'movie': {
+#                 'id': movie.id,
+#                 'title': movie.name,
+#                 'description': movie.description,
+#                 # 'release_date': movie.release_date,
+#             }}
+#     return JsonResponse(data)
     
