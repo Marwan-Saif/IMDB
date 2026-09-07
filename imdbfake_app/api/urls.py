49adf8c094fa73b68f38a6f2d67fa221e121032a
Marwan Saif <merosaif837@gmail.com>
2026-09-07 03:12:15 +0300

    chore : creat CRUD operations using (Serializer & ClassView )

--- imdbfake_app/api/urls.py
+++ imdbfake_app/api/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from imdbfake_app.api.views import movie_list, movie_detail
+# from imdbfake_app.api.views import  movie_list, movie_detail
+from imdbfake_app.api.views import MovieDetailAV, MovieListAV
 
 
 urlpatterns=[
-    path('', movie_list, name='movie_list'),
-    path('<int:pk>/', movie_detail, name='movie_detail'),
+    path('', MovieListAV.as_view(), name='movie_list'),
+    path('<int:pk>/', MovieDetailAV.as_view(), name='movie_detail'),
 ]
--- imdbfake_app/api/views.py
+++ imdbfake_app/api/views.py
@@ -1,45 +1,83 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+# from rest_framework.decorators import api_view
+from rest_framework.views import APIView, Http404
 from imdbfake_app.api.serializers import MovieSerializer
 from imdbfake_app.models import Movie
 
-
-@api_view(['GET', 'POST'])
-def movie_list(request):
-    if request.method == 'GET':
+class MovieListAV(APIView):
+    def get(self, request):
         movies = Movie.objects.all()
-        serializer=MovieSerializer (movies, many=True)
+        serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer=MovieSerializer(data=request.data)
+
+    def post(self, request):
+        serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def movie_detail(request, pk):
-    if request.method == 'GET':
+class MovieDetailAV(APIView):
+    def get_object(self, pk):
         try:
-            movie = Movie.objects.get(pk=pk)
-            serializer = MovieSerializer(movie)
-            return Response(serializer.data)
+            return Movie.objects.get(pk=pk)
         except Movie.DoesNotExist:
-            return Response({"error": "Movie not found"}, status=404)
-    if request.method == 'PUT':
-        try:
-            movie = Movie.objects.get(pk=pk)
-        except Movie.DoesNotExist:
-            return Response({"error": "Movie not found"}, status=404)
-        serializer = MovieSerializer(movie, data=request.data) #if i didnt pass the movie instance, it will create a new movie instead of updating the existing one
+            raise Http404
+
+    def get(self, request, pk):
+        movie = self.get_object(pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        movie = self.get_object(pk)
+        serializer = MovieSerializer(movie, data=request.data, partial=True) #partial means that i want put fun to act as patch &&&& if i didnt pass the movie instance, it will create a new movie instead of updating the existing one
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=400)
-    if request.method == 'DELETE':
-        try:
-            movie = Movie.objects.get(pk=pk)
-        except Movie.DoesNotExist:
-            return Response({"error": "Movie not found"}, status=404)
+
+    def delete(self, request, pk):
+        movie = self.get_object(pk)
         movie.delete()
         return Response(status=204)
+    
+# @api_view(['GET', 'POST'])
+# def movie_list(request):
+#     if request.method == 'GET':
+#         movies = Movie.objects.all()
+#         serializer=MovieSerializer (movies, many=True)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer=MovieSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=201)
+#         return Response(serializer.errors, status=400)
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def movie_detail(request, pk):
+#     if request.method == 'GET':
+#         try:
+#             movie = Movie.objects.get(pk=pk)
+#             serializer = MovieSerializer(movie)
+#             return Response(serializer.data)
+#         except Movie.DoesNotExist:
+#             return Response({"error": "Movie not found"}, status=404)
+#     if request.method == 'PUT':
+#         try:
+#             movie = Movie.objects.get(pk=pk)
+#         except Movie.DoesNotExist:
+#             return Response({"error": "Movie not found"}, status=404)
+#         serializer = MovieSerializer(movie, data=request.data) #if i didnt pass the movie instance, it will create a new movie instead of updating the existing one
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=400)
+#     if request.method == 'DELETE':
+#         try:
+#             movie = Movie.objects.get(pk=pk)
+#         except Movie.DoesNotExist:
+#             return Response({"error": "Movie not found"}, status=404)
+#         movie.delete()
+#         return Response(status=204)
