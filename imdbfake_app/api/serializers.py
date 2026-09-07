f0f73d1b040cc81d193450bd130bfd9fb44b52e3
Marwan Saif <merosaif837@gmail.com>
2026-09-07 13:40:50 +0300

    chore : validation

--- imdbfake_app/api/serializers.py
+++ imdbfake_app/api/serializers.py
@@ -5,11 +5,14 @@ from django.core import serializers
 from rest_framework import serializers
 from imdbfake_app.models import Movie
 
-
+def describtion_length(value): # Custom validator function  &&& this function will be used in the serializer field as a validator && can be used with more than one field  &&& should be defined outside the serializer class
+    if len(value) < 10:
+        raise serializers.ValidationError("Description must be at least 10 characters long.")
+ 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
-    description = serializers.CharField()
+    description = serializers.CharField(validators=[describtion_length])
     active = serializers.BooleanField(default=True)
     
     def create(self, validated_data):
@@ -20,3 +23,12 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+    
+    def vslidate_name(self, value):  #Field-level validation  Fun naming convention: validate_<field_name>
+        if len(value) < 2:
+            raise serializers.ValidationError("Name must be at least 2 characters long.")
+        return value
+    def validate(self, data):    #Object-level validation    
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Name and description must be different.")
+        return data
